2f58561ae49d9f465e8952656cc736940ccde85e
barmenski <1kiloom@gmail.com>
2025-01-21 19:55:48 +0300

    fix: add update in task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,15 +1,26 @@
 const fs = require('node:fs/promises');
 const { copyFile } = require('node:fs/promises');
 const path = require('path');
-const process = require('process');
+const { constants } = require('fs');
 
 (async () => {
+  try {
+    await fs.access(path.join(__dirname, 'files-copy'), constants.F_OK);
+    await fs.rmdir(path.join(__dirname, 'files-copy'), {
+      recursive: true,
+      force: true,
+    });
+  } catch {
+    console.error('file does not exists');
+  }
+
   await fs.mkdir(path.join(__dirname, 'files-copy'), {
     recursive: true,
   });
   const files = await fs.readdir(path.join(__dirname, 'files'), {
     withFileTypes: true,
   });
+
   for (const file of files) {
     const ext = path.extname(file.name);
     const name = path.parse(file.name).name;
@@ -21,11 +32,11 @@ const process = require('process');
 
       function (error) {
         if (error) throw error;
-      }
+      },
     );
     await copyFile(
       path.join(__dirname, 'files', name + ext),
-      path.join(__dirname, 'files-copy', name + ext)
+      path.join(__dirname, 'files-copy', name + ext),
     );
     console.log(name + ext + '  ' + size.size + 'b' + ' | ' + 'copied!');
   }
